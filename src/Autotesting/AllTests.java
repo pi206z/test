0e0c6f29dff0ac86aa154ba496b9de018dc7a50a
user <user@user-VirtualBox>
2018-03-21 21:41:03 +0400

    new tests

--- src/Autotesting/AllTests.java
+++ src/Autotesting/AllTests.java
@@ -6,8 +6,6 @@ import org.junit.BeforeClass;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
-
 public class AllTests {
 	
 	private static FirefoxDriver driver;
@@ -17,130 +15,112 @@ public class AllTests {
 	public static void openBrowser(){
 	System.setProperty("webdriver.gecko.driver", "geckodriver");
 	driver = new FirefoxDriver();
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 	}
 @Test
-public void ElementsVisibility() {
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-	driver.findElement(By.xpath("//h1")).click();
-	driver.findElement(By.xpath("//h2")).click();
-	driver.findElement(By.xpath("//label")).click();
+public void ElementsVisibilityAndFrontValidation() {
+	String ResultHTML = "http://localhost:8080/OSAGOCalculator/Result.html"; //Адрес страницы результата
+	driver.get("http://localhost:8080/OSAGOCalculator/");//Переход на страницу калькулятора
+	int RandomNegativeValue = (int)Math.random()*(-100); //Генератор случайного отрицательного числа
+	int RandomPositiveValueMoreThanPossible = (int)Math.random()*700; //Генератор случайного положительного числа
+	driver.findElement(By.xpath("//h1")).isDisplayed(); //Отображение элемента на странице
+	driver.findElement(By.xpath("//h2")).isDisplayed();
+	driver.findElement(By.xpath("//label")).click(); //Нажатие на элемент
 	driver.findElement(By.xpath("//label[2]")).click();
 	driver.findElement(By.xpath("//label[3]")).click();
-	driver.findElement(By.xpath("//h2[2]")).click();
-	driver.findElement(By.xpath("//h2[3]")).click();
-	driver.findElement(By.xpath("//h2[4]")).click();
+	driver.findElement(By.xpath("//h2[2]")).isDisplayed();
+	driver.findElement(By.xpath("//h2[3]")).isDisplayed();
+	driver.findElement(By.xpath("//h2[4]")).isDisplayed();
 	driver.findElement(By.name("Power")).click();
 	driver.findElement(By.name("Age")).click();
 	driver.findElement(By.name("Experience")).click();
-	driver.findElement(By.id("submit")).click();
-	driver.navigate().refresh();
-}
-@Test
-public void ValidationFrontRegionUsage() {
-	String ResultHTML = "http://localhost:8080/OSAGOCalculator/Result.html";
-	driver.get("http://localhost:8080/OSAGOCalculator/");
+	driver.findElement(By.id("submit")).isDisplayed();
+	driver.navigate().refresh(); // обновление страницы
 	driver.findElement(By.xpath("//label")).click();
 	driver.findElement(By.xpath("//label[2]")).click();
 	driver.findElement(By.xpath("//label[3]")).click();
 	driver.findElement(By.id("submit")).click();
-	String CalcHTML = driver.getCurrentUrl();
-	assert CalcHTML != ResultHTML: "Redirect to ResultHTML is succeed, error";
-	driver.navigate().refresh();	
-}
-@Test
-public void ValidationFrontPower() {
-	String ResultHTML = "http://localhost:8080/OSAGOCalculator/Result.html";
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-	    driver.findElement(By.name("Power")).sendKeys("900");
-	    driver.findElement(By.name("Age")).sendKeys("50");
-	    driver.findElement(By.name("Experience")).sendKeys("18");
-	    driver.findElement(By.id("submit")).click();
-		String CalcHTML = driver.getCurrentUrl();
-		assert CalcHTML != ResultHTML: "Redirect to ResultHTML is succeed, error";
-		driver.navigate().refresh();
-}
-@Test
-public void ValidationFrontAge() {
-	String ResultHTML = "http://localhost:8080/OSAGOCalculator/Result.html";
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-	driver.findElement(By.name("Power")).sendKeys("70");
-	driver.findElement(By.name("Age")).sendKeys("150");
-	driver.findElement(By.name("Experience")).sendKeys("12");
-	driver.findElement(By.id("submit")).click();
-	String CalcHTML = driver.getCurrentUrl();
-	assert CalcHTML != ResultHTML: "Redirect to ResultHTML is succeed, error";
+	String CalcHTML = driver.getCurrentUrl(); //Получение текущей ссылки
+	assert CalcHTML != ResultHTML: "Ошибка. Осуществлен переход на страницу результата"; //обработка исключения при переходе на страницу результатов
 	driver.navigate().refresh();
-}
-@Test
-public void ValidationFrontExperience() {
-	String ResultHTML = "http://localhost:8080/OSAGOCalculator/Result.html";
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-    driver.findElement(By.name("Power")).sendKeys("70");
-    driver.findElement(By.name("Age")).sendKeys("80");
-    driver.findElement(By.name("Experience")).sendKeys("-2");
+	driver.findElement(By.name("Power")).sendKeys(String.valueOf(RandomNegativeValue));
+    driver.findElement(By.name("Age")).sendKeys(String.valueOf(RandomNegativeValue));
+    driver.findElement(By.name("Experience")).sendKeys(String.valueOf(RandomNegativeValue));
     driver.findElement(By.id("submit")).click();
-    String CalcHTML = driver.getCurrentUrl();
-	assert CalcHTML != ResultHTML: "Redirect to ResultHTML is succeed, error";
+	assert CalcHTML != ResultHTML: "Ошибка. Осуществлен переход на страницу результата";
+	driver.navigate().refresh();
+	driver.findElement(By.name("Power")).sendKeys(String.valueOf(RandomPositiveValueMoreThanPossible));
+    driver.findElement(By.name("Age")).sendKeys(String.valueOf(RandomPositiveValueMoreThanPossible));
+    driver.findElement(By.name("Experience")).sendKeys(String.valueOf(RandomPositiveValueMoreThanPossible));
+    driver.findElement(By.id("submit")).click();
+	assert CalcHTML != ResultHTML: "Ошибка. Осуществлен переход на страницу результата";
 	driver.navigate().refresh();
 }
 @Test
-public void ValidationBackMultipleChooseRegion() {
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-	driver.findElement(By.xpath("//label")).click();
+public void ValidationBackIncorrectInput() {
+	driver.get("http://localhost:8080/OSAGOCalculator/"); //Переход на страницу калькулятора
+	driver.findElement(By.xpath("//label")).click(); //Нажатие на элемент
 	driver.findElement(By.xpath("//label[2]")).click();
 	driver.findElement(By.xpath("//label[3]")).click();
-    driver.findElement(By.name("Power")).sendKeys("70");
+    driver.findElement(By.name("Power")).sendKeys("70"); //ввод значения в поле Power
     driver.findElement(By.name("Age")).sendKeys("50");
     driver.findElement(By.name("Experience")).sendKeys("20");
     driver.findElement(By.id("submit")).click();
-    WebElement ValidationMessage = driver.findElement(By.xpath("//h1"));
-    System.out.println("Пользователю выдано сообщение: " + ValidationMessage.getText());
-    driver.navigate().back();
-    driver.navigate().refresh();
-}
-@Test
-public void CalculationForMoscow() {
-	driver.get("http://localhost:8080/OSAGOCalculator/");
-	driver.findElement(By.xpath("//label")).click();
-    driver.findElement(By.name("Power")).sendKeys("70");
-    driver.findElement(By.name("Age")).sendKeys("30");
-    driver.findElement(By.name("Experience")).sendKeys("5");
+    WebElement ValidationMessage = driver.findElement(By.xpath("//h1"));//Поиск элемента по XPath 
+    System.out.println("Пользователю выдано сообщение об ошибке: " + ValidationMessage.getText()); //вывод в консоль сообщение об ошибке (содержмиое тега h1) 
+    driver.navigate().back(); //Переход на предыдущую страницу
+    driver.navigate().refresh(); //обновление страницы
+    driver.findElement(By.xpath("//label")).click(); 
+    driver.findElement(By.name("Power")).sendKeys("50");
+    driver.findElement(By.name("Age")).sendKeys("20");
+    driver.findElement(By.name("Experience")).sendKeys("25");
     driver.findElement(By.id("submit")).click();
-    WebElement ResultMessage = driver.findElement(By.xpath("//h1"));
-    System.out.println("Для Москвы " + ResultMessage.getText());
+    WebElement ValidationMessage2 = driver.findElement(By.xpath("//h1"));
+    System.out.println("Пользователю выдано сообщение об ошибке: " + ValidationMessage2.getText());
     driver.navigate().back();
     driver.navigate().refresh();
-}
-@Test
-public void CalculationForSPB() {
-	driver.findElement(By.xpath("//label[2]")).click();
-	driver.findElement(By.name("Power")).sendKeys("114");
-	driver.findElement(By.name("Age")).sendKeys("20");
-	driver.findElement(By.name("Experience")).sendKeys("2");
-	driver.findElement(By.id("submit")).click();
-	driver.findElement(By.xpath("//h1")).click();
-	WebElement ResultMessage = driver.findElement(By.xpath("//h1"));
-	System.out.println("Для СПб " +ResultMessage.getText());
-	driver.navigate().back();
-	driver.navigate().refresh();
+    driver.findElement(By.xpath("//label[2]")).click();
+    driver.findElement(By.xpath("//label")).click();
+    driver.findElement(By.name("Power")).sendKeys("60");
+    driver.findElement(By.name("Age")).sendKeys("40");
+    driver.findElement(By.name("Experience")).sendKeys("20");
+    driver.findElement(By.id("submit")).click();
+    WebElement ValidationMessage3 = driver.findElement(By.xpath("//h1"));
+    System.out.println("Пользователю выдано сообщение об ошибке: " + ValidationMessage3.getText());
+    driver.navigate().back();
+    driver.navigate().refresh();
+    driver.findElement(By.xpath("//label[2]")).click();
+    driver.findElement(By.name("Power")).sendKeys("60");
+    driver.findElement(By.name("Age")).sendKeys("18");
+    driver.findElement(By.name("Experience")).sendKeys("1");
+    driver.findElement(By.id("submit")).click();
+    WebElement ValidationMessage4= driver.findElement(By.xpath("//h1"));
+    System.out.println("Пользователю выдано сообщение об ошибке: " + ValidationMessage4.getText()); 
 }
 @Test 
-public void CalculationForUfa() {
-	driver.findElement(By.xpath("//label[3]")).click();
-	driver.findElement(By.name("Power")).sendKeys("200");
-	driver.findElement(By.name("Age")).sendKeys("18");
-	driver.findElement(By.name("Experience")).sendKeys("0");
+public void CalculationTest() {
+	driver.get("http://localhost:8080/OSAGOCalculator/"); //Переход на страницу калькулятора
+	int PowerUp = 550; int PowerDown = 150; //задание диапазон значений для рандомайзера (Мощность ТС)
+	int AgeUp = 99; int AgeDown = 23; // -==- (Возраст водителя)
+	int ExperienceUp = 8; int ExperienceDown = 0; // (Стаж водителя) 
+	for(int counter = 1; counter <= 10; counter = counter+1) { // Цикл. 10 расчетов со случайно введенными значениями
+	String RandomPowerValue = String.valueOf(PowerDown + (int)(Math.random()*PowerUp)) ;
+	String RandomAgeValue = String.valueOf(AgeDown + (int)(Math.random()*(AgeUp-AgeDown))) ;
+	String RandomExperienceValue = String.valueOf(ExperienceDown + (int)(Math.random()*ExperienceUp)) ;
+	driver.findElement(By.xpath("//label[3]")).click(); //Нажатие на элемент
+	driver.findElement(By.name("Power")).sendKeys(RandomPowerValue); //Ввод рандомного значения в поле Power
+	driver.findElement(By.name("Age")).sendKeys(RandomAgeValue);
+	driver.findElement(By.name("Experience")).sendKeys(RandomExperienceValue);
 	driver.findElement(By.id("submit")).click();
 	driver.findElement(By.xpath("//h1")).click();
 	WebElement ResultMessage = driver.findElement(By.xpath("//h1"));
-	System.out.println("Для Уфы " + ResultMessage.getText());
-    driver.navigate().back();
-    driver.navigate().refresh();
+	System.out.println("Расчет №" + counter + "  " + ResultMessage.getText() + ". Использованные данные: Мощность ТС - " + RandomPowerValue + " Возраст водителя - " + RandomAgeValue + " Стаж вождения - " + RandomExperienceValue); //вывод результатов расчета
+    driver.navigate().back(); //переход на предыдущую страницу в браузере 
+    driver.navigate().refresh(); //Обновление страницы
+	}
 }
 @AfterClass
 public static void ClosingTestRun() {
 driver.quit();
 }
 }
-		
